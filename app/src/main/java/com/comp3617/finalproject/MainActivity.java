3cdc89152d70c619376dd425f0dcc63cab366492
Patrick <patrick.zhang1@telus.com>
2017-06-12 12:06:12 -0700

    set onclick for item of pokemon

--- app/src/main/java/com/comp3617/finalproject/MainActivity.java
+++ app/src/main/java/com/comp3617/finalproject/MainActivity.java
@@ -2,8 +2,11 @@ package com.comp3617.finalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +22,15 @@ public class MainActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.pokemonList);
         List<Pokemon> pokemonList = PokemonFactory.create();
         PokemonListAdapter adapter = new PokemonListAdapter(this, pokemonList);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(getApplicationContext(),
+                        "Click ListItem Number " + position, Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
         listView.setAdapter(adapter);
     }
 }
