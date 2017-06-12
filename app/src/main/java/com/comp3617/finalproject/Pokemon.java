c71de8ca9e0bb7ca869920627041494c0ee6674b
Patrick <patrick.zhang1@telus.com>
2017-06-11 19:19:19 -0700

    add pokemon class and pokemon skill class

--- app/src/main/java/com/comp3617/finalproject/Pokemon.java
+++ app/src/main/java/com/comp3617/finalproject/Pokemon.java
@@ -1,10 +1,23 @@
 package com.comp3617.finalproject;
 
+import java.util.List;
+
 /**
  * Created by Patrick on 6/11/2017.
  */
 
 public class Pokemon {
+
+    private int name;
+
+    private int photo;
+
+    private int icon;
+
+    private int hp;
+
+    private List<PokemonSkill> skills;
+
     public Pokemon () {
 
     }
---
+++ app/src/main/java/com/comp3617/finalproject/PokemonSkill.java
@@ -0,0 +1,15 @@
+package com.comp3617.finalproject;
+
+/**
+ * Created by Patrick on 6/11/2017.
+ */
+
+public class PokemonSkill {
+    private int name;
+    private int damage;
+    private int icon;
+
+    public PokemonSkill (){
+
+    }
+}
