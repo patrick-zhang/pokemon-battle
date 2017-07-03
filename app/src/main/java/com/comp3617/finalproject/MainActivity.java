d2de688b32abdcf6517b249fea80c821fe729b4f
Patrick Zhang <patrick.zhang1@telus.com>
2017-07-03 15:51:19 -0700

    add menu items and activities for add / view pokemon

--- app/src/main/java/com/comp3617/finalproject/MainActivity.java
+++ app/src/main/java/com/comp3617/finalproject/MainActivity.java
@@ -1,7 +1,10 @@
 package com.comp3617.finalproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -23,15 +26,34 @@ public class MainActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.pokemonList);
         List<Pokemon> pokemonList = PokemonFactory.create();
         PokemonListAdapter adapter = new PokemonListAdapter(this, pokemonList);
+        listView.setAdapter(adapter);
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
-                Toast.makeText(getApplicationContext(),
-                        "Click ListItem Number " + position, Toast.LENGTH_LONG)
-                        .show();
+                Intent intent = new Intent(MainActivity.this, PokemonDetailActivity.class);
+                startActivity(intent);
             }
         });
-        listView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    public void performAction(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.addItem: {
+                Intent intent = new Intent(this, AddPokemonActivity.class);
+                startActivity(intent);
+            }
+            default : {
+              break;
+            }
+        }
     }
 }
