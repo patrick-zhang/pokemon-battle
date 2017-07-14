330e6397a1889be2f255dce56ba3c847fc2a2df9
Patrick Zhang T898061 <patrick.zhang1@telus.com>
2017-07-14 00:02:53 -0700

    get name from api

--- app/src/main/java/com/comp3617/finalproject/AddPokemonActivity.java
+++ app/src/main/java/com/comp3617/finalproject/AddPokemonActivity.java
@@ -2,6 +2,7 @@ package com.comp3617.finalproject;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +11,16 @@ import android.widget.EditText;
 import com.comp3617.finalproject.model.Pokemon;
 import com.comp3617.finalproject.model.PokemonFactory;
 import com.comp3617.finalproject.model.RealmManager;
-import com.comp3617.finalproject.utils.HttpUtil;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Random;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class AddPokemonActivity extends AppCompatActivity {
 
@@ -26,13 +36,11 @@ public class AddPokemonActivity extends AppCompatActivity {
                 EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
                 String pokemonName = nameEditText.getText().toString();
                 if (pokemonName.isEmpty()) {
-                    pokemonName = HttpUtil.getPokemonName();
+                    HttpUtil httpUtil = new HttpUtil();
+                    httpUtil.execute("https://pokeapi.co/api/v1/pokedex/1/");
+                } else {
+                    addPokemon(pokemonName);
                 }
-                Pokemon pokemon = PokemonFactory.build(pokemonName);
-                RealmManager.addPokemon(pokemon);
-                Intent intent = new Intent();
-                setResult(Activity.RESULT_OK, intent);
-                finish();
                 break;
             }
             case R.id.btnBackInAdd: {
@@ -42,6 +50,50 @@ public class AddPokemonActivity extends AppCompatActivity {
                 break;
             }
             default: break;
+        }
+    }
+
+    private void addPokemon(String pokemonName) {
+        Pokemon pokemon = PokemonFactory.build(pokemonName);
+        RealmManager.addPokemon(pokemon);
+        Intent intent = new Intent();
+        setResult(Activity.RESULT_OK, intent);
+        finish();
+    }
+
+    public class HttpUtil extends AsyncTask<String, Void, String> {
+
+        OkHttpClient client = new OkHttpClient();
+
+        @Override
+        protected String doInBackground(String... params) {
+            Request.Builder builder = new Request.Builder();
+            builder.url(params[0]);
+            Request request = builder.build();
+
+            try {
+                Response response = client.newCall(request).execute();
+                return response.body().string();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            JSONObject json = null;
+            try {
+                json = new JSONObject(s);
+                JSONArray pokemonArray = json.getJSONArray("pokemon");
+                int size = pokemonArray.length();
+                JSONObject pokemon = pokemonArray.getJSONObject(Math.abs(new Random().nextInt(size)));
+                String pokemonName = pokemon.getString("name");
+                addPokemon(pokemonName);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 }
