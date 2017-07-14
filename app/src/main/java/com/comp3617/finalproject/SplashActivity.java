1a51e6102afbdf1a8f3512f9ba72388037f6311c
Patrick <patrick.zhang1@telus.com>
2017-07-14 12:47:03 -0700

    add background music

--- app/src/main/java/com/comp3617/finalproject/SplashActivity.java
+++ app/src/main/java/com/comp3617/finalproject/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.comp3617.finalproject;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,7 @@ import com.comp3617.finalproject.model.SkillFactory;
 import com.comp3617.finalproject.utils.PrefUtil;
 
 public class SplashActivity extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +25,25 @@ public class SplashActivity extends AppCompatActivity {
             RealmManager.addPokemon(PokemonFactory.getInitialPokemonList());
             prefUtil.setBoolean(PrefUtil.PREF_KEY_INITIALIZED, true);
         }
+        mediaPlayer = MediaPlayer.create(this, R.raw.pokemon_music);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mediaPlayer.start();
     }
 
     public void onClick(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.stop();
+        mediaPlayer.release();
     }
 }
