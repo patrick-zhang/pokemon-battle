80cc8da0dea878e48938b0243932a68e135ab617
Patrick <patrick.zhang1@telus.com>
2017-07-14 13:43:56 -0700

    update battle splash view

--- app/src/main/java/com/comp3617/finalproject/BattleSplashActivity.java
+++ app/src/main/java/com/comp3617/finalproject/BattleSplashActivity.java
@@ -2,6 +2,7 @@ package com.comp3617.finalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class BattleSplashActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class BattleSplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battle_splash);
+    }
+
+    public void onClick(View view) {
+
     }
 }
